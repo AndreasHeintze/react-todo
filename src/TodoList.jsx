c0f272a6ce90450a3918804128a931141c272898
Andreas Heintze <andreas@heintze.se>
2025-07-05 10:09:21 +0200

    fix: small changes

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,25 +1,36 @@
-import {useContext, useMemo} from "react"
-import {TodoContext} from "./contexts/TodoContext"
-import Todo from "./Todo"
+import { useContext, useMemo } from 'react'
+import { TodoContext } from './contexts/TodoContext'
+import Todo from './Todo'
 
 export default function TodoList() {
-  const {todos} = useContext(TodoContext)
+  const { todos } = useContext(TodoContext)
 
   const sortedTodos = useMemo(() => {
-    const uncompleted = todos.filter(todo => !todo.completed).sort((a, b) => a.sortOrder - b.sortOrder)
-    const completed = todos.filter(todo => todo.completed).sort((a, b) => b.completedAt - a.completedAt)
-    return [...uncompleted, ...completed]
+    return todos.filter((todo) => !todo.completed).sort((a, b) => a.sortOrder - b.sortOrder)
   }, [todos])
 
+  const completedTodos = todos.filter((todo) => todo.completed).sort((a, b) => b.completedAt - a.completedAt)
+
   return (
-    <ul className="@container">
-      {sortedTodos.map((todo) => {
-        return (
-          <li key={todo.id}>
-            <Todo todo={todo} />
-          </li>
-        )
-      })}
-    </ul>
+    <>
+      <ul className="@container space-y-2">
+        {sortedTodos.map((todo) => {
+          return (
+            <li key={todo.id}>
+              <Todo todo={todo} />
+            </li>
+          )
+        })}
+      </ul>
+      <ul className="@container mt-2 space-y-2">
+        {completedTodos.map((todo) => {
+          return (
+            <li key={todo.id}>
+              <Todo todo={todo} />
+            </li>
+          )
+        })}
+      </ul>
+    </>
   )
 }
